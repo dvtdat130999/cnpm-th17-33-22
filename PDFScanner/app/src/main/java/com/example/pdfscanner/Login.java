bfb8181531289220d459e0ebe9467c913ba97dc8
Dat <50757817+dvtdat130999@users.noreply.github.com>
2019-10-27 14:04:46 +0700

    add login, register

--- PDFScanner/app/src/main/java/com/example/pdfscanner/Login.java
+++ PDFScanner/app/src/main/java/com/example/pdfscanner/Login.java
@@ -2,13 +2,55 @@ package com.example.pdfscanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+    TextView txtvRegister;
 
+    EditText edtUsername,edtPassword;
+    Button btnSignIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        txtvRegister=(TextView)findViewById(R.id.txtvRegister);
+        edtUsername=(EditText)findViewById(R.id.edtUsername);
+        edtPassword=(EditText)findViewById(R.id.edtPassword);
+        btnSignIn=(Button)findViewById(R.id.btnSignIn);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username=edtUsername.getText().toString();
+                String password=edtPassword.getText().toString();
+
+                if(username!=""&&password!="")
+                {
+                    android.app.AlertDialog.Builder builder=new android.app.AlertDialog.Builder(Login.this);
+                    builder.setMessage("Login success!").create().show();
+
+
+                }
+                else
+                {
+                    android.app.AlertDialog.Builder builder=new android.app.AlertDialog.Builder(Login.this);
+                    builder.setMessage("Login failed!").setNegativeButton("Retry",null).create().show();
+
+                }
+            }
+        });
+        txtvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this, Register.class));
+
+            }
+        });
     }
 }
--- PDFScanner/app/src/main/java/com/example/pdfscanner/Register.java
+++ PDFScanner/app/src/main/java/com/example/pdfscanner/Register.java
@@ -2,13 +2,92 @@ package com.example.pdfscanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
-public class Register extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Register extends AppCompatActivity implements View.OnClickListener{
+    TextView txtvLogin;
+    EditText edtFName,edtLName,edtAddress,edtPhone,edtUsername,edtPassword,edtBirthdate;
+    Button btnSignUp,btnSelect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        txtvLogin=(TextView)findViewById(R.id.txtvLogin);
+        btnSignUp=(Button)findViewById(R.id.btnSignUp);
+        btnSelect=(Button)findViewById(R.id.btnSelect);
+
+
+        edtFName=(EditText)findViewById(R.id.edtFirstName);
+        edtLName=(EditText)findViewById(R.id.edtLastName);
+        edtAddress=(EditText)findViewById(R.id.edtAddress);
+        edtPhone=(EditText)findViewById(R.id.edtPhone);
+        edtBirthdate=(EditText)findViewById(R.id.edtBirthdate);
+        edtUsername=(EditText)findViewById(R.id.edtUsername);
+        edtPassword=(EditText)findViewById(R.id.edtPassword);
+
+        btnSignUp.setOnClickListener(this);
+        txtvLogin.setOnClickListener(this);
+        btnSelect.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view){
+        switch(view.getId()) {
+            case R.id.btnSignUp:
+                String fname=edtFName.getText().toString();
+                String lname=edtLName.getText().toString();
+                String address=edtAddress.getText().toString();
+                String phone=edtPhone.getText().toString();
+                String birthdate=edtBirthdate.getText().toString();
+                String username=edtUsername.getText().toString();
+                String password=edtPassword.getText().toString();
+
+                if(fname!=""&&lname!=""&&address!=""&&phone!=""&&birthdate!=""&&username!=""&&password!="")
+                {
+                    android.app.AlertDialog.Builder builder=new android.app.AlertDialog.Builder(Register.this);
+                    builder.setMessage("Register success!").create().show();
+                }
+                if(fname!=""&&lname!=""&&address!=""&&phone!=""&&birthdate!=""&&username!=""&&password!="")
+                {
+                    android.app.AlertDialog.Builder builder=new android.app.AlertDialog.Builder(Register.this);
+                    builder.setMessage("Register failed!").create().show();
+                }
+                break;
+            case R.id.txtvLogin:
+                startActivity(new Intent(this, Login.class));
+                break;
+            case R.id.btnSelect:
+                pickDate();
+                break;
+
+        }
+    }
+    private void pickDate(){
+        final Calendar calendar=Calendar.getInstance();
+        int day=calendar.get(Calendar.DATE);
+        int month=calendar.get(Calendar.MONTH);
+        int year=calendar.get(Calendar.YEAR);
+        DatePickerDialog datePickerDialog=new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                calendar.set(i,i1,i2);
+                SimpleDateFormat simpleDateFormat=new SimpleDateFormat("dd/MM/yyyy");
+                edtBirthdate.setText(simpleDateFormat.format(calendar.getTime()));
+            }
+        },year,month,day);
+        datePickerDialog.show();
     }
 }
